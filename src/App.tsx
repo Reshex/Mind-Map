d225ebf0d91fb4046c83eab0c2eda39793cdb567
Bar .A <111001969+Reshex@users.noreply.github.com>
2024-10-07 21:05:35 +0300

    new files commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+function App() {
+
+  return (
+    <h1>Gzub</h1>
+  )
+}
+
+export default App
---
+++ src/firebase.ts
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDAOtc93LQLS0yF-1dOVgd8YpiVIsutovc",
+  authDomain: "mind-map-da8fb.firebaseapp.com",
+  projectId: "mind-map-da8fb",
+  storageBucket: "mind-map-da8fb.appspot.com",
+  messagingSenderId: "396419413613",
+  appId: "1:396419413613:web:c1c751b626ac9bc4263164",
+  measurementId: "G-1CL3C1035T",
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
