2fcbf97e0b448029593be1863699072199e69c3d
Bar .A <111001969+Reshex@users.noreply.github.com>
2024-10-16 18:37:48 +0300

    little changes for readabiliity

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,16 @@
+import * as functions from "firebase-functions/v1";
 import * as admin from "firebase-admin";
+
 admin.initializeApp();
-import { validateToken } from "./httpRequests";
 
-// Export the validateToken function to be deployed as a Firebase Cloud Function
-export { validateToken };
+exports.onUserDelete = functions.auth.user().onDelete(async (user: functions.auth.UserRecord) => {
+  const uid = user.uid;
 
+  try {
+    const userDocRef = admin.firestore().collection("users").doc(uid);
+    await userDocRef.delete();
+    console.log(`Successfully deleted user data for UID: ${uid}`);
+  } catch (error) {
+    console.error(`Error deleting user data for UID: ${uid}`, error);
+  }
+});
---
+++ functions/src/types/UserType.ts
@@ -0,0 +1,7 @@
+export default interface User {
+    uid: string,
+    name: string,
+    lastName: string,
+    email: string,
+    password: string
+}
